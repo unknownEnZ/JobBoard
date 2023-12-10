ea6e7e07d4741367e4e98066b69cc70614c0382b
unknownEnZ <2200647B@student.tp.edu.sg>
2023-12-10 10:55:22 +0800

    Added User Authentication

--- JobBoard/Server/Models/ApplicationUser.cs
+++ JobBoard/Server/Models/ApplicationUser.cs
@@ -4,5 +4,8 @@ namespace JobBoard.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
